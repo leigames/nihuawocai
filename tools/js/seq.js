f2a304a95cb6be26ac0fec8a8b4830a46cfcaad7
leigames <pointless826@gmail.com>
2024-08-14 16:07:46 +0800

    Add files via upload

--- tools/js/seq.js
+++ tools/js/seq.js
@@ -1,15 +1,64 @@
-function random_num(Min, Max) {
+function randomNum(Min, Max) {
     var num = Min + Math.round(Math.random() * (Max - Min));
     return num;
 }
 
-function show_seq() {
-    num = Number(document.getElementById("num-select").value);
+function changeNumMode() {
+    use_namelist = document.getElementById("use_namelist").checked;
+    if (use_namelist) {
+        document.getElementById("num_input").style.display = "none";
+        document.getElementById("namelist_input").style.display = "block";
+    } else {
+        document.getElementById("num_input").style.display = "block";
+        document.getElementById("namelist_input").style.display = "none";
+    }
+    updateLengthSelect()
+}
+
+function getNameList() {
+    use_namelist = document.getElementById("use_namelist").checked;
+    if (!use_namelist) return;
+    namelist = document.getElementById("namelist").value.replace(/^\s+|\s+$/g, "").replace(/\n\s+/g, "\n").split("\n");
+    return namelist;
+}
+
+function getNumOfPlayers() {
+    use_namelist = document.getElementById("use_namelist").checked;
+    if (use_namelist) {
+        return getNameList().length;
+    }
+    return Number(document.getElementById("num_select").value);
+}
+
+function updateLengthSelect() {
+    length_select = document.getElementById("length_select");
+    // reset
+    length_select.innerHTML = "";
+
+    num = getNumOfPlayers();
+
+    // if num < 6 or num > 16, disable the showSeq button
+    if (num < 6 || num > 16) {
+        document.getElementById("showSeq").disabled = true;
+    } else {
+        document.getElementById("showSeq").disabled = false;
+    }
+
+    // add options
+    for (var i = 6; i <= num; i += 2) {
+        length_select.innerHTML += "<option value=\"" + i + "\">" + i + " 人</option>";
+    }
+    length_select.value = String((num % 2 == 0) ? num : num - 1);
+}
+
+function showSeq() {
+    num = getNumOfPlayers();
+    length = Number(document.getElementById("length_select").value);
     var init = [];
     var outputsort = [];
     var rand_map = [];
     for (var i = 0; i < num; i++) {
-        init[i] = random_num(1, num);
+        init[i] = randomNum(1, num);
         for (var j = 0; j < i; j++) {
             if (init[i] == init[j]) {
                 i--;
@@ -18,7 +67,7 @@ function show_seq() {
         }
     }
     for (var i = 0; i < num; i++) {
-        outputsort[i] = random_num(1, num);
+        outputsort[i] = randomNum(1, num);
         for (var j = 0; j < i; j++) {
             if (outputsort[i] == outputsort[j]) {
                 i--;
@@ -27,7 +76,7 @@ function show_seq() {
         }
     }
     for (var i = 0; i < num; i++) {
-        rand_map[i] = random_num(1, num);
+        rand_map[i] = randomNum(1, num);
         for (var j = 0; j < i; j++) {
             if (rand_map[i] == rand_map[j]) {
                 i--;
@@ -41,9 +90,9 @@ function show_seq() {
         move_vector[i + 1] = num + 1 - move_vector[i];
     }
     var res_array = new Array();
-    console.log(JSON.stringify(move_vector));
-    console.log(JSON.stringify(init));
-    console.log(JSON.stringify(outputsort));
+    // console.log(JSON.stringify(move_vector));
+    // console.log(JSON.stringify(init));
+    // console.log(JSON.stringify(outputsort));
     // initialize
     for (var i = 0; i < num; i++) {
         res_array[i] = new Array();
@@ -56,25 +105,57 @@ function show_seq() {
             res_array[outputsort[j] - 1][i] = (init[j] + move_vector[i]) % num;
         }
     }
-    console.log(JSON.stringify(res_array));
+    // console.log(JSON.stringify(res_array));
     ret = document.getElementById("output");
-    n = document.getElementById("no");
-    ret.innerHTML = "";
-    n.innerHTML = "";
+    ret.innerHTML = "<ol id='result'></ol>";
+    res = document.getElementById("result");
+    // n = document.getElementById("no");
+    // n.innerHTML = "";
+
+    // Random layer
     for (var i = 0; i < num; i++) {
         for (var j = 0; j < num; j++) {
-            ret.innerHTML += rand_map[res_array[i][j]] + " ";
+            res_array[i][j] = rand_map[res_array[i][j]];
         }
-        ret.innerHTML += "<br>"
     }
+    sort = document.getElementById("sort").checked;
+    use_namelist = document.getElementById("use_namelist").checked;
+
+    if (sort) {
+        res_array.sort(function(a, b) {
+            return a[0] - b[0];
+        });
+    }
+    console.log(JSON.stringify(res_array));
+
+    if (use_namelist) {
+        namelist = getNameList();
+        for (var i = 0; i < num; i++) {
+            for (var j = 0; j < num; j++) {
+                res_array[i][j] = "\"" + namelist[res_array[i][j] - 1] + "\"";
+            }
+        }
+    }
+
     for (var i = 0; i < num; i++) {
-        n.innerHTML += "[" + (i + 1) + "]<br>"
+        let tmp_str = "";
+        tmp_str += "<li>";
+        for (var j = 0; j < length; j++) {
+            tmp_str += res_array[i][j];
+            tmp_str += (j == length - 1) ? "" : ",";
+        }
+        tmp_str.innerHTML += "</li>"
+        res.innerHTML += tmp_str;
     }
 }
+/*for (var i = 0; i < num; i++) {
+    n.innerHTML += "[" + (i + 1) + "]<br>"
+}*/
+
 
 function reset() {
     ret = document.getElementById("output");
-    n = document.getElementById("no");
+    // n = document.getElementById("no");
     ret.innerHTML = "生成的序列将显示在这里";
-    n.innerHTML = "";
+    // n.innerHTML = "";
 }
